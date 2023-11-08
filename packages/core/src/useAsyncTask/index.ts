468f2d259d6a16f0e817e1d8a41a5e1eacb7d482
chenzhigang <979703986@qq.com>
2023-11-08 13:34:12 +0800

    chore: vue-use

--- packages/core/src/useAsyncTask/index.ts
+++ packages/core/src/useAsyncTask/index.ts
@@ -1,7 +1,73 @@
-export interface UseAsyncTaskOptions {}
+import { isFunction } from '@daysnap/utils'
+import type { AnyPromiseFn } from '@daysnap/types'
+import { onBeforeMount, ref, type Ref } from 'vue'
 
-export const useAsyncTask = (
-  options?: UseAsyncTaskOptions
-) => {
-  console.log(options)
+export interface UseAsyncTaskOptions<T extends AnyPromiseFn> {
+  /**
+   * 是否立即执行 onBeforeMount 执行，默认 false
+   */
+  immediate?: boolean
+
+  /**
+   * 立即执行的时候 第一次需要传递的参数
+   */
+  immediateParams?: Parameters<T> | (() => Parameters<T>)
+
+  /**
+   * 初始数据
+   */
+  initialValue?: Awaited<ReturnType<T>>
+
+  /**
+   * 捕获了异常，是否继续抛出，默认 false
+   */
+  throwError?: boolean | (() => boolean)
+
+  /**
+   * 错误回调，返回 false 或者 undefined 的时候，阻断默认错误 error 逻辑
+   */
+  onError?: (err: unknown) => boolean | undefined | Promise<boolean | undefined>
+}
+
+/**
+ * 异步任务执行
+ */
+export function useAsyncTask<T extends AnyPromiseFn>(task: T, options?: UseAsyncTaskOptions<T>) {
+  const { initialValue, immediate, throwError, onError, immediateParams } = options ?? {}
+
+  const data = ref(initialValue) as Ref<Awaited<ReturnType<T>>>
+  const error = ref<unknown>()
+  const loading = ref(false)
+
+  const trigger = async (...args: Parameters<T>) => {
+    try {
+      error.value = undefined
+      loading.value = true
+      data.value = await task(...args)
+    } catch (err) {
+      if ((await onError?.(err)) ?? true) {
+        error.value = err
+        const isThrow = isFunction(throwError) ? throwError() : throwError
+        if (isThrow) {
+          throw err
+        }
+      }
+    } finally {
+      loading.value = false
+    }
+  }
+
+  onBeforeMount(async () => {
+    if (immediate) {
+      const args = isFunction(immediateParams) ? immediateParams() : immediateParams ?? []
+      await trigger(...(args as any))
+    }
+  })
+
+  return {
+    data,
+    error,
+    loading,
+    trigger,
+  }
 }
--- playground/src/views/demo/use-async-task/UseAsyncTaskView.vue
+++ playground/src/views/demo/use-async-task/UseAsyncTaskView.vue
@@ -1,13 +1,50 @@
 <template>
   <HorView>
-    <p></p>
+    <br />
+    <dl>
+      <dt>设置</dt>
+      <dd><br /></dd>
+      <dd>
+        <span>执行报错：</span>
+        <br />
+        <VanSwitch v-model="isError" />
+      </dd>
+      <dd>
+        <span>是否继续抛出Error：</span>
+        <br />
+        <VanSwitch v-model="throwError" />
+      </dd>
+      <dd><br /></dd>
+      <dt>请求结果:</dt>
+      <dd><br /></dd>
+      <dd v-if="loading">请求中...</dd>
+      <dd v-else-if="error" style="color: rgb(200, 0, 0)">错误 => {{ error }}</dd>
+      <dd v-else style="color: rgb(0, 200, 0)">成功 => {{ data }}</dd>
+    </dl>
+    <br />
+    <VanButton @click="trigger()" :loading="loading">执行</VanButton>
   </HorView>
 </template>
 
 <script setup lang="ts">
+  import { sleep } from '@daysnap/utils'
   import { useAsyncTask } from '@daysnap/vue-use'
 
-  const {} = useAsyncTask()
+  const throwError = ref(false)
+  const isError = ref(false)
+  const { data, error, loading, trigger } = useAsyncTask(
+    async () => {
+      await sleep(1000)
+      if (isError.value) {
+        throw new Error('请求报错')
+      }
+      return Date.now()
+    },
+    {
+      immediate: true,
+      throwError: () => throwError.value,
+    },
+  )
 </script>
 
 <style lang="scss" scoped>
