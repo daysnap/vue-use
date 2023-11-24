05516f862ead884f1e95c60f8ad749df1b1b9fb9
chenzhigang <979703986@qq.com>
2023-11-24 16:35:10 +0800

    feat: use transfer

--- packages/core/src/useTransfer/index.ts
+++ packages/core/src/useTransfer/index.ts
@@ -1,21 +1,42 @@
-import { useRouter } from 'vue-router'
+import { useRouter, type RouteLocationRaw } from 'vue-router'
 
 export interface TransferOptions {
+  /**
+   * 是否中转
+   */
   disabled?: boolean
+
+  /**
+   * 路由配置 推荐使用 [to]
+   */
   replace?: boolean
   query?: Record<string, any>
   params?: Record<string, any>
   path?: string
-  fn?(options: TransferOptions, ...args: any[]): void
+
+  /**
+   * 路由配置
+   */
+  to?: RouteLocationRaw
+
+  /**
+   * 函数执行
+   */
+  fn?(options: TransferOptions, ...args: any[]): void | Promise<void>
   [prop: string]: any
 }
 
 export function useTransfer() {
   const router = useRouter()
   return async (options: TransferOptions = {}, ...args: any[]) => {
-    const { disabled, path, replace = false, fn, query, ...rest } = options
+    const { disabled, path, replace = false, fn, query, to, ...rest } = options
 
     if (disabled) {
+      return
+    }
+
+    if (to) {
+      router.push({ path, replace, ...rest })
       return
     }
 
